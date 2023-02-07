0ef0911cfd166c103b0b143cefdc09c093858268
Sammmy22 <sambhavc225@outlook.com>
2023-02-07 17:37:47 +0530

    Redesigning the inputs

--- script.js
+++
@@ -1,7 +0,0 @@
-const slider = document.querySelector("#slider");
-
-slider.addEventListener("change", function () {
-  console.log(this.value);
-
-  document.querySelector("#slider-value").innerHTML = this.value;
-});
